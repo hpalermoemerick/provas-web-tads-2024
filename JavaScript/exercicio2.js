e93d1038ddb4d9a16f2bce2dfb352585216c9032
hpe-palermo <h>
2024-10-07 22:23:04 -0300

    Aula 07-10-2024

--- JavaScript/exercicio2.js
+++ JavaScript/exercicio2.js
@@ -4,6 +4,9 @@
 const form = document.querySelector('form');
 const resp = document.querySelector('h2');
 
+let mouse = document.createEvent('MouseEvent');
+mouse.initMouseEvent('mousedown', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const nome = form.inNome.value;
@@ -12,5 +15,18 @@ form.addEventListener('submit', (e) => {
     const nota3 = Number(form.inNota3.value);
     const nota4 = Number(form.inNota4.value);
     const media = (nota1 + nota2 + nota3 + nota4) / 4;
-    resp.textContent = `A média do aluno ${nome} é: ${media.toFixed(1)}`;
+
+    let situacao = '';
+    if (media >= 7) {
+        situacao = 'Aprovado(a)';
+        resp.style.color = 'blue';
+    } else if (media >= 4) {
+        situacao = 'Recuperação';
+        resp.style.color = 'green';
+    } else {
+        situacao = 'Reprovado(a)';
+        resp.style.color = 'red';
+    }
+
+    resp.textContent = `A média do(a) aluno(a) ${nome} é: ${media.toFixed(1)}. Aluno(a) ${situacao}`;
 });
